64ef8d86fd5a2c9a10a9e4e92126e3e93f76b810
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-09-01 11:01:35 +0100

    Auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // Logout
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logout Success',
+            'status'=>'success'
+        ], 200);
+    }
+    
+    // LoggedUser
+    public function logged_user(){
+        $loggeduser = auth()->user();
+        return response([
+            'user'=>$loggeduser,
+            'message' => 'Logged User Data',
+            'status'=>'success'
+        ], 200);
+    }
+
+
+}
