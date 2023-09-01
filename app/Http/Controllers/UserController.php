f3b24983d4047b14cc11fc80f13db34136f80e3e
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-09-01 11:01:26 +0100

    user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //Change Password
+
+    public function change_password(Request $request){
+        $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+        $loggeduser = auth()->user();
+        $loggeduser->password = Hash::make($request->password);
+        $loggeduser->save();
+        return response([
+            'message' => 'Password Changed Successfully',
+            'status'=>'success'
+        ], 200);
+    }
+}
